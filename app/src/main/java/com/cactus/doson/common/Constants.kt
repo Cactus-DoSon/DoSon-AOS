65e921c3fcc79301bfe822fec5364501ca727b48
jihyun <leejudy1017@naver.com>
2022-01-30 13:03:19 +0900

    url 변경

--- app/src/main/java/com/cactus/doson/common/Constants.kt
+++ app/src/main/java/com/cactus/doson/common/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val RETROFIT_TAG = "retrofit_tag"
 
     /** Remote **/
-    const val BASE_URL = "http://jwyang.shop:8080/"
+    const val BASE_URL = "http://13.125.168.69:8080/"
     const val JWT = "jwt"
     const val HEADER = "X-AUTH-TOKEN"
 
